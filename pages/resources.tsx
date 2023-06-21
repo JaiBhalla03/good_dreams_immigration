9ef11d915abd6ffeb6522339f41e3a6d6c5289cd
jai <jai.bhalla2021@vitstudent.ac.in>
2023-06-21 15:11:59 +0530

    removing the type error in the resources page

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -30,7 +30,10 @@ const Resources = () => {
                 </div>
             </motion.div>
             <div className={'bg-gray-100 flex px-2 md:px-20 w-full gap-4 justify-between py-4 md:pt-16 md:py-5'}>
-                <div className={'pt-2 md:pt-6 px-2 md:px-8 bg-white w-full pb-8 flex flex-col gap-8'}>
+                <motion.div
+                    initial={{x: -1000, opacity: 0}}
+                    animate={{x: 0, opacity: 1, transition: {duration: 1}}}
+                    className={'pt-2 md:pt-6 px-2 md:px-8 bg-white w-full pb-8 flex flex-col gap-8'}>
                     <h1
 
                         className={'relative text-3xl md:text-4xl font-semibold md:font-bold text-red-800'}
@@ -92,7 +95,7 @@ const Resources = () => {
                             link={'https://www.canada.ca/en/immigration-refugees-citizenship/services/application/medical-police/police-certificates/how.html'}
                         />
                     </ul>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
