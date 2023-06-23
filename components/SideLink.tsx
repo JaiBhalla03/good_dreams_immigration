0e0b6a3e8df30a0a58bac85eab62540d107d7dda
jai <jai.bhalla2021@vitstudent.ac.in>
2023-06-23 11:14:58 +0530

    Adding all the services page and making them responsive

--- components/SideLink.tsx
+++ components/SideLink.tsx
@@ -10,7 +10,10 @@ const SideLink = () => {
         >
             <ul className={'bg-white border-l border-t border-r border-gray-500 flex flex-col'}>
                 <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'permanent-resident'}>Permanent Resident(PR)</Link>
-                <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'temporary'}>Temporary Resident Visa(TRV)</Link>
+                <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'entrepreneurship-visa-program'}>Entrepreneurship Visa Program</Link>
+                <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'student-visa'}>Student Visa</Link>
+                <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'super-visa'}>Super Visa</Link>
+                <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'visitor-visa'}>Visitor Visa</Link>
                 <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'family-sponsorship'}>Family Sponsorship</Link>
                 <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'work-permit'}>Work Permit</Link>
                 <Link className={'flex items-center justify-center p-4 text-xl border-b hover:bg-red-800 hover:text-white duration-300 transition-all border-gray-500 text-gray-500'} href={'lmia'}>LMIA</Link>
